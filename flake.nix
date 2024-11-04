2882d77798ef82a6846c0a2f1419b0bcb6e181a1
NemNix <NemNix@noreply.codeberg.org>
2024-11-04 12:13:16 +0100

    cleaned flake

--- flake.nix
+++ flake.nix
@@ -1,62 +1,54 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-  };
+  inputs = { nixpkgs.url = "nixpkgs/nixos-unstable"; };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    systems = [
-      "aarch64-linux"
-      "x86_64-linux"
-    ];
+  outputs = { self, nixpkgs, ... } @ inputs:
+    let
+      systems = [ "aarch64-linux" "x86_64-linux" ];
+    in
+    rec {
 
-    in rec {
-    nixosConfigurationsForAllSystems = system: {
-      "offline-installer-${system}" = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        inherit system;
-        modules = [
-          ./nix-cfg/configuration.nix
-          ({ pkgs, lib, ... }: {
-            nixpkgs.overlays = [
-              (self: super: {
-                calamares-nixos-extensions = super.calamares-nixos-extensions.overrideAttrs (oldAttrs: rec {
-                  patches = oldAttrs.patches or [] ++ [ 
-                    ./patches/welcome.patch 
-                    ./patches/nixos.patch
-                  ];
-                });
-              })
-            ];
-          })
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix"
-          ({ pkgs, config, ... }: {
-            isoImage = {
-              contents = [
-                {
+      # Generate iso configurations for each system
+      iso = builtins.mapAttrs (name: config: config.config.system.build.isoImage) nixosConfigurations;
+      # Generate nixosConfigurations for each system
+      nixosConfigurations = builtins.foldl' (acc: system: acc // (nixosConfigurationsForAllSystems system)) { } systems;
+
+      nixosConfigurationsForAllSystems = system: {
+        "offline-installer-${system}" = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          inherit system;
+
+          modules = [
+            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix"
+            ./nix-cfg/configuration.nix
+
+            {
+              nixpkgs.overlays = [
+                (self: super: {
+                  calamares-nixos-extensions = super.calamares-nixos-extensions.overrideAttrs (oldAttrs: {
+                    patches = oldAttrs.patches or [ ] ++ [
+                      ./patches/welcome.patch
+                      ./patches/nixos.patch
+                    ];
+                  });
+                })
+              ];
+            }
+
+            ({ config, ... }: {
+              isoImage = {
+                contents = [
+                  {
                     source = ./nix-cfg;
                     target = "/nix-cfg";
-                }
-              ];
-              storeContents = [ 
-                config.system.build.toplevel
-              ];
-              includeSystemBuildDependencies = true;
-              squashfsCompression = "gzip -Xcompression-level 1";
-            };
-          })
-        ];
+                  }
+                ];
+                includeSystemBuildDependencies = true; # ISO OFFLINE
+                storeContents = [ config.system.build.toplevel ];
+                squashfsCompression = "gzip -Xcompression-level 1";
+              };
+            })
+          ];
+        };
       };
     };
-
-    # Generate nixosConfigurations for each system
-    nixosConfigurations = builtins.foldl' (acc: system: acc // (nixosConfigurationsForAllSystems system)) { } systems;
-
-    # Generate iso configurations for each system
-    iso = builtins.mapAttrs (name: config: config.config.system.build.isoImage) nixosConfigurations;
-  };
 }
